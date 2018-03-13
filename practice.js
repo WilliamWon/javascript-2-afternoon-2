043b0f25b5ce329b41084722961de9cdb369d8ac
William Won <Wiwon123@Williams-MacBook-Pro.local>
2018-03-13 18:36:31 -0500

    JS-afternoon-2

--- practice.js
+++ practice.js
@@ -18,7 +18,9 @@ var arr = [10,20,30];
 */
 
 //Code Here
-
+function first(arr){
+  return arr[0];
+}
 
 
 ////////// PROBLEM 2 //////////
@@ -33,7 +35,9 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[2];
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -48,7 +52,11 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
+function looper(arr){
+  for(var i=0; i< arr.length; i++){
+    alert(arr[i]);
+  }
+}
 
 
 ////////// PROBLEM 4 //////////
@@ -63,7 +71,11 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
-
+function reversedLooper(arr){
+  for(var i=arr.length-1;i>-1;i--){
+    alert(arr[i]);
+  }
+}
 
 
 ////////// PROBLEM 5 //////////
@@ -78,7 +90,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+var newNums = [];
 
+function evenFinder(arr){
+  for(var i=0;i<arr.length;i++){
+    if(arr[i]%2 === 0){
+      newNums.push(arr[i]);
+    }
+  }
+  return newNums;
+}
 
 
 ////////// EXTRA PRACTICE PROBLEMS BELOW //////////
@@ -96,6 +117,17 @@ var numbersArray = [1,2,34,54,55,34,32,11,19,17,54,66,13];
 
 //Code Here
 
+function divider(arr){
+  var numbersArray1 = [[], []];
+  for(var i=0;i<arr.length;i++){
+    if(arr[i] % 2 === 0){
+      numbersArray1[0].push(arr[i]);
+    }else {
+      numbersArray1[1].push(arr[i]);
+    }
+  }
+  return numbersArray1;
+}
 
 
 ////////// PROBLEM 7 //////////
@@ -116,6 +148,15 @@ var getRandomArbitrary = function() {
 */
 
 //Code Here
+function finder(arr){
+  var rando = getRandomArbitrary();
+  for(var i=0;i<arr.length;i++){
+    if(arr[i] === rando){
+      return true;
+    }
+  }
+  return false;
+}
 
 
 
@@ -143,7 +184,11 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 */
 
 //Code Here
-
+function removeItem(array,item){
+  if(array.includes(item)){
+    array.splice(array[item],1);
+  }
+}
 
 
 ////////// PROBLEM 9 //////////
@@ -153,7 +198,13 @@ var myGroceryList = ['chips', 'pizza', 'hotpockets', 'MtnDew', 'corndogs'];
 */
 
 //Code Here
-
+function maker(){
+  var testArray = [];
+  for(var i = 1; i<216; i++){
+    testArray.push(i);
+  }
+  return testArray;
+}
 
 
 ////////// PROBLEM 10 //////////
@@ -169,7 +220,11 @@ var numbers = [5, '9', 16, 19, '25', '34', 48];
 */
   
 //Code Here
-
+function addTen(array){
+  for(var i=0;i<array.length;i++){
+    array[i]+=10;
+  }
+}
 
 
 ////////// PROBLEM 11 //////////
